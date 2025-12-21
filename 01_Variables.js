b0d8e3c0dec93345701142b98859a6611902e353
at1730951-prog <at1730951@gmail.com>
2025-12-21 21:55:58 +0530

    datatypes

--- 01_Variables.js
+++ 01_Variables.js
@@ -1,1 +1,8 @@
+const accountID = 144553
+let accountEmail = "aaryanpnc55@gmail.com"
+var accountPassword = "12345"
+accountcity = "Jaipur"
 
+//accountID = 2
+
+console.log(accountID);
---
+++ 01_basics/03_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newer version 
+// alert(3+3) // we are not using nodejs not browser
+
+console.log(3
+    +
+    3) // code readibolity sholuld be high
+
+console.log("Aaryan rajput")    
+
+// primitive datatypes
+
+let name = "Aaryan rajput"
+let age = 18
+let isloggedIn = false
+let state; //undefined
+
+// number => 2 to power 53
+// bright  numer is big like trading and share market
+// string => ""
+// boolean => true/false
+// null (empty) = standalone value as well as datatypes
+// undefined 
+// symbol => unique
+
+// object
+
+console.log("type of Aaryan") //string
+console.log("type of age") //number
+console.log("type of null") //object
+console.log("undefined") // undefined
+
