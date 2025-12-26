4314ca18020bce567a352edb489e16bcd9c74d44
at1730951-prog <at1730951@gmail.com>
2025-12-26 15:23:32 +0000

    conversion of datatypes

---
+++ 01_basics/04_conversionOPeration.js
@@ -0,0 +1,36 @@
+//let score = 33
+
+//console.log(typeof score); //number
+//console.log(typeof(score)); //number
+
+//let score = "33"
+
+//console.log(typeof score); // string
+//console.log(typeof(score)); //string
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber); // number
+
+//let score = "33abc"
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber); // Number
+//console.log(valueInNumber); // NaN
+
+// "33"  =>convert=> NaN
+// "33abc" => NaN
+// true => 1;  false => 0;
+
+//let isLoggedIn = "aaryan"
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 =>convert =>true; 0 =>false
+// "" => false
+// "aaryan"  => true
+
+let someNumber = 33
+
+let stringNumber = string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
