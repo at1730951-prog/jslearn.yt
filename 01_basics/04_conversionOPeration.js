ad4a4b64b8b27e52152f252e041f4467480cdfd2
at1730951-prog <at1730951@gmail.com>
2025-12-26 16:09:43 +0000

    0peration

--- 01_basics/04_conversionOPeration.js
+++ 01_basics/04_conversionOPeration.js
@@ -27,10 +27,44 @@
 // "" => false
 // "aaryan"  => true
 
-let someNumber = 33
+//let someNumber = 33
 
-let stringNumber = string(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//let stringNumber = string(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+//************************* operation ****************************/
+
+//let value = 3
+//let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);   //4
+//console.log(2-2);   //0
+//console.log(2*2);   //4
+//console.log(2**3);   //8
+//console.log(2/2);   //1
+//console.log(2%3);    //2
+
+//let str1 = "hello"
+//let str2 = " Aaryan rajput"
+//let str3 = str1+str2
+//console.log(str3); //hello Aaryan rajput
+
+//console.log("1"+2);  // 12
+//console.log(1+"2");  // 12
+//console.log("1"+2+2); // 122
+//console.log(1+2+"2");  // 32
+
+//console.log(+true)  // 1
+//console.log(+"")    //  0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter); //101
 
 
