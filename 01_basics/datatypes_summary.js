344858f9ed20154dd03e9b162f0eaac9ea8b80dd
at1730951-prog <at1730951@gmail.com>
2025-12-27 14:34:34 +0000

    datatypes_summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,36 @@
+// ## primitive  => call by value
+
+// $$ 7 types :: string, Number, Boolean, null, undefined, symbol, BigIint=> scientfic value
+
+
+//const score = 39
+const scoreValue = 100.8  //number
+
+//const isLoggedIn = false
+const outsideTemp = null //object
+//let userEmail;
+
+//const id = Symbol('123')
+//const anotherId = symbol('123')
+
+//console.log(id === anotherId);  // false
+
+//const bigNumber = 234567899098776575n  //bigint
+
+// ## Non primitive =>call by reference
+
+// Array, Objects, Function
+
+//const heros = ["shaktiman", "naagraj", "doga"]  //Array
+
+//let myobj = {
+  //  name:"Aaryan rajput",
+  //  age:19,
+//}
+
+//const myFunction = function(){
+  //  console.log("Hello world");
+
+//}
+
+console.log(typeof scoreValue);
