349b718d963cc43332fb498a12626ac2221ff65f
at1730951-prog <at1730951@gmail.com>
2025-12-28 04:10:14 +0000

    maths

---
+++ 01_basics/07_num-math.js
@@ -0,0 +1,43 @@
+//const score = 400
+//console.log(score);  //400
+
+const balance = new Number(100)
+//console.log(balance);   // [Number:100]
+
+//console.log(balance.toString().length);  //3
+//console.log(balance.toFixed(2)); // 100.00 used to build e-commerce website
+
+//const otherNumber = 123.8956
+//console.log(otherNumber.toPrecision(4));  // 123.9
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString()); // 1,000,000
+//console.log(hundreds.toLocaleString('en-In'));  //10,00,000
+
+
+//++++++++++++++++++++++++++ maths => by default ++++++++++++++++++++++++++++++++++
+
+//console.log(Math); //object [math] {}
+//console.log(Math.abs(-4));  // 4
+//console.log(Math.abs(4));   // 4
+//console.log(Math.round(4.5));  // 5
+//console.log(Math.ceil(4.2));   // 5
+//console.log(Math.floor(4.9));  // 4
+//console.log(Math.min(6, 6, 7, 8, 9)); //6
+//console.log(Math.max(9, 8, 7, 6, 5));  // 9
+
+//console.log(Math.random());  // give random value exist b/w 0 and 1
+//console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random() *10) + 1);
+
+//const min = 10
+//const max = 20
+//console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+
+
